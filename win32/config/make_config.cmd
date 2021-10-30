a648fa2fa1146037b837d223a85161cab3d97ba8
Paulo Custodio <pauloscustodio@gmail.com>
2021-10-30 15:28:20 +0100

    Do not define WIN32, it causes errors building in Unix

--- win32/config/make_config.cmd
+++ win32/config/make_config.cmd
@@ -6,7 +6,7 @@ set today=%date%
 set today=%today:-=%
 rem if not exist config.h (
   echo #define PREFIX "/usr/local/share/z88dk"	>  config.h
-  echo #define WIN32 1							>> config.h
+rem   echo #define WIN32 1							>> config.h
   echo #define Z88DK_VERSION "MSC build-%today%"	>> config.h
   echo Created %dir%\config.h
 rem ) else (
